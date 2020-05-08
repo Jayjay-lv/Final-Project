959d8717f9dbcba10ff91fb39b585d7be7d2656f
Jayjay <jayjayvenegas@gmail.com>
2020-05-08 09:43:03 -0700

    Added menu
    Used stuff we learned from snake tutorial to add a menu

---
+++ PictureGame/PictureGame/GameManager.swift
@@ -0,0 +1,14 @@
+//
+//  GameManager.swift
+//  PictureGame
+//
+//  Created by jayjay venegas on 5/8/20.
+//  Copyright © 2020 jayjay venegas. All rights reserved.
+//
+import SpriteKit
+import Foundation
+
+
+class GameManager {
+    
+}
