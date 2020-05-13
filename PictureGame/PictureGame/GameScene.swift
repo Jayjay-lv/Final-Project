dd04b8dc341c2d1875758a7a4dfbe6bf129781cd
Jayjay <jayjayvenegas@gmail.com>
2020-05-12 21:00:10 -0700

    New Grid
    Started from scratch using a different method to make a grid. Did not work.

--- PictureGame/PictureGame/GameScene.swift
+++ PictureGame/PictureGame/GameScene.swift
@@ -10,59 +10,15 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
+    override func didMove(to: SKView) {
+        if let grid = Grid(blockSize: 40.0, rows:5, cols:5) {
+            grid.position = CGPoint (x:frame.midX, y:frame.midY)
+            addChild(grid)
 
-    var game: GameManager!
-
-    var gameBG: SKShapeNode!
-    var gameArray: [(node: SKShapeNode, x: Int, y: Int)] = []
-    
-    
- 
-    override func didMove(to view: SKView) {
-        game = GameManager(scene:self)
-        
-      initializeGameView()
-    }
-    
-    
-    private func initializeGameView() {
-        
-        let width = Int(frame.size.width - 200)
-        let height = Int(frame.size.height - 300)
-        let rect = CGRect(x: -width / 2, y: -height / 2, width: width, height: height)
-        gameBG = SKShapeNode(rect: rect, cornerRadius: 0.02)
-        gameBG.fillColor = SKColor.darkGray
-        gameBG.zPosition = 2
-        gameBG.isHidden = true
-        self.addChild(gameBG)
-        
-        createGameBoard(width: Int(width), height: Int(height))
-    }
-    
-    private func createGameBoard(width: Int, height: Int) {
-        let cellWidth: CGFloat = CGFloat(width) / 5
-        let cellHeight: CGFloat = CGFloat(height) / 10.0
-        let numRows = 10
-        let numCols = 5
-        var x = CGFloat(width / -2) + (cellWidth / 2)
-        var y = CGFloat(height / 2) - (cellWidth / 2)
-        for i in 0...numRows - 1 {
-            for j in 0...numCols - 1 {
-                let cellNode = SKShapeNode(rectOf: CGSize(width: cellWidth, height: cellHeight))
-                cellNode.strokeColor = SKColor.black
-                cellNode.zPosition = 2
-                cellNode.position = CGPoint(x: x, y: y)
-                gameArray.append((node: cellNode, x: i, y: j))
-                gameBG.addChild(cellNode)
-                x += cellWidth
-            }
-            x = CGFloat(width / -2) + (cellWidth / 2)
-            y -= cellWidth
+            let gamePiece = SKSpriteNode(imageNamed: "Spaceship")
+            gamePiece.setScale(0.0625)
+            gamePiece.position = grid.gridPosition(row: 1, col: 0)
+            grid.addChild(gamePiece)
         }
     }
-  
-
 }
-
-
--- PictureGame/PictureGame/Grid.swift
+++ PictureGame/PictureGame/Grid.swift
@@ -5,56 +5,78 @@
 //  Created by jayjay venegas on 5/11/20.
 //  Copyright © 2020 jayjay venegas. All rights reserved.
 //
-
 import SpriteKit
 
-class Grid: SKSpriteNode {
-    let rows = 8
-    let columns = 10
 
-    var cellWidth = 0
-    var cellHeight = 0
+class Grid:SKSpriteNode {
+    var rows:Int!
+    var cols:Int!
+    var blockSize:CGFloat!
 
-    
-    var gridArray = [[Picture]]()
+    convenience init?(blockSize:CGFloat,rows:Int,cols:Int) {
+        guard let texture = Grid.gridTexture(blockSize: blockSize,rows: rows, cols:cols) else {
+            return nil
+        }
+        self.init(texture: texture, color:SKColor.clear, size: texture.size())
+        self.blockSize = blockSize
+        self.rows = rows
+        self.cols = cols
+        
+        self.isUserInteractionEnabled = true
+    }
+
+    class func gridTexture(blockSize:CGFloat,rows:Int,cols:Int) -> SKTexture? {
+        let size = CGSize(width: CGFloat(cols)*blockSize+1.0, height: CGFloat(rows)*blockSize+1.0)
+        UIGraphicsBeginImageContext(size)
+
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return nil
+        }
+        let bezierPath = UIBezierPath()
+        let offset:CGFloat = 0.5
+
+        for i in 0...cols {
+            let x = CGFloat(i)*blockSize + offset
+            bezierPath.move(to: CGPoint(x: x, y: 0))
+            bezierPath.addLine(to: CGPoint(x: x, y: size.height))
+        }
+        for i in 0...rows {
+            let y = CGFloat(i)*blockSize + offset
+            bezierPath.move(to: CGPoint(x: 0, y: y))
+            bezierPath.addLine(to: CGPoint(x: size.width, y: y))
+        }
+        SKColor.white.setStroke()
+        bezierPath.lineWidth = 1.0
+        bezierPath.stroke()
+        context.addPath(bezierPath.cgPath)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return SKTexture(image: image!)
+    }
+
+    func gridPosition(row:Int, col:Int) -> CGPoint {
+        let offset = blockSize / 2.0 + 0.5
+        let x = CGFloat(col) * blockSize - (blockSize * CGFloat(cols)) / 2.0 + offset
+        let y = CGFloat(rows - row - 1) * blockSize - (blockSize * CGFloat(rows)) / 2.0 + offset
+        return CGPoint(x:x, y:y)
+    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    
-        let touch = touches.first!
-        let location = touch.location(in: self)
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-
-            isUserInteractionEnabled = true
-
-            cellWidth = Int(size.width) / columns
-        cellHeight = Int(size.height) / rows
-        
-        populateGrid()
-
-    }
-    
-    func populateGrid() {
-      for gridX in 0..<columns {
-          gridArray.append([])
-          for gridY in 0..<rows {
-
-              addPictureAtGrid(x:gridX, y:gridY)
-          }
-      }
-        
-    }
-    func addPictureAtGrid(x: Int, y: Int) {
-        let picture = Picture()
-        let gridPosition = CGPoint(x: x*cellWidth, y: y*cellWidth)
-        picture.position = gridPosition
-        
-      //  picture.isMatched = true
-
-        addChild(picture)
-
-        gridArray[x].append(picture)
+        for touch in touches {
+            let position = touch.location(in:self)
+            let node = atPoint(position)
+            if node != self {
+                let action = SKAction.rotate(by:CGFloat.pi*2, duration: 1)
+                node.run(action)
+            }
+            else {
+                let x = size.width / 2 + position.x
+                let y = size.height / 2 - position.y
+                let row = Int(floor(x / blockSize))
+                let col = Int(floor(y / blockSize))
+                print("\(row) \(col)")
+            }
+        }
     }
 }
